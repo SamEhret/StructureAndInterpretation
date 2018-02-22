3031fe677948d8cbb3c10375f88bcab7268628af
Sam Ehret <ehret.sam@yahoo.com>
2018-02-22 10:45:46 -0600

    added question

--- chapter1/1.06.scm
+++ chapter1/1.06.scm
@@ -1,3 +1,7 @@
+; why can't "if" be created as an ordinary procedure instead of as a
+; special form? Using a "new-if" for finding square roots, explain why
+; this will not work
+
 The square root computations will get stuck in a loop for the else-clause
 since this will keep being evaluated, calling the procedure again. This
 is because the condition statement is wrapped in a function.
